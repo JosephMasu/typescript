d7171e1e15aeac6cceceaf3f210f3ba3b41c73e0
Joe Masu <josephmuhindo089@gmail.com>
2024-07-06 13:20:19 +0300

    fetch Data

--- src/Exercises.ts
+++ src/Exercises.ts
@@ -932,3 +932,40 @@ let arraryNumbers = createArray<number>(10, 200);
 console.log(generateStringArray(5, 'Masu'))
 console.log(arraryNumbers);
 console.log(arraryStrings);
+
+//fetch Data
+const url = 'https://www.course-api.com/react-tours-project';
+type Tour ={
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+}
+
+async function fetchData(url: string):Promise<Tour[]> {
+  try {
+    const response = await fetch(url);
+
+    // Check if the request was successful.
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data:Tour[] = await response.json();
+    console.log(data);
+    
+    return data;
+  } catch (error) {
+    const errMsg =
+      error instanceof Error ? error.message : 'there was an error...';
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+const tour = await fetchData(url);
+tour.map((tour)=>{
+  console.log(tour.name);
+  
+});
