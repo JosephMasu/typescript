c582bb2b889204d4f8e4f9052b6370a035d26569
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-01-01 21:04:39 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="./tasks" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
+    
     <a href="./tasks" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
+    
     <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
