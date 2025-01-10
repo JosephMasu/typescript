fee3a5a46c4235a23471215efecde92084a02ec3
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-01-10 22:04:29 +0200

    Update tasks.ts

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,5 +1,4 @@
 const taskForm = document.querySelector<HTMLFormElement>('.form');
-
 const formInput = document.querySelector<HTMLInputElement>('.form-input');
 
 const taskListElement = document.querySelector<HTMLUListElement>('.list');
